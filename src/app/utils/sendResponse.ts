221539556f8b95991458c58bab447bb0dcfff536
Mehedi Hassan Barat <mehedi.haassan1@gmail.com>
2024-06-11 22:28:12 +0600

    catchAsync and sendResponse done

---
+++ src/app/utils/sendResponse.ts
@@ -0,0 +1,20 @@
+import { Response } from "express";
+
+type TResponse<T> = {
+    statusCode: number;
+    success: boolean;
+    message: string;
+    data: T
+}
+
+
+const sendResponse = <T>(res: Response, response: TResponse<T>) => {
+    return res.status(response?.statusCode).send({
+        success: response?.success,
+        message: response?.message,
+        data: response?.data
+    })
+}
+
+
+export default sendResponse;
