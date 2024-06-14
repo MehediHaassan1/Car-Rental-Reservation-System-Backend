be2287628b2c6bd7266efd8b3b09c5e7e4b4cb92
Mehedi Hassan Barat <mehedi.haassan1@gmail.com>
2024-06-14 11:29:44 +0600

    validation error and app error done

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -4,6 +4,8 @@ import { ZodError } from "zod";
 import { TErrorSources } from "../interface/error";
 import handleZodError from "../errors/handleZodError";
 import handleDuplicateError from "../errors/handleDuplicateError";
+import handleValidationError from "../errors/handleValidationError";
+import AppError from "../errors/AppError";
 
 const globalErrorHandler = (
     error: any,
@@ -32,13 +34,27 @@ const globalErrorHandler = (
         statusCode = simplifiedError.statusCode;
         message = simplifiedError.message;
         errorSources = simplifiedError.errorSources;
+    } else if (error.name === 'ValidationError') {
+        const simplifiedError = handleValidationError(error);
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message;
+        errorSources = simplifiedError.errorSources;
+    } else if (error instanceof AppError) {
+        statusCode = error?.statusCode;
+        message = error.message;
+        errorSources = [
+            {
+                path: '',
+                message: error?.message,
+            },
+        ];
     }
 
     return res.status(statusCode).json({
         success: false,
         message,
         errorSources,
-        // error,
+        error,
         // stack: error.stack
     })
 }
